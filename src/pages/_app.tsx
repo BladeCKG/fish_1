f2fd30e159383ad644fa64e722540d1213a40178
BladeCKG <neverdreamagain9106@gmail.com>
2023-11-15 12:41:42 -0800

    bug fix in rainbow kit ui css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { chains, wagmiConfig } from "config/wagmi";
 import type { AppProps } from "next/app";
 import { useEffect, useState } from "react";
 import "styles/globals.css";
-import "styles/rainbowkit.css";
+import '@rainbow-me/rainbowkit/styles.css';
 import { WagmiConfig } from "wagmi";
 import { ChakraProvider } from "@chakra-ui/react";
 import { env } from "../config/env";
